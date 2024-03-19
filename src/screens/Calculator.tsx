b3103375954c4fb84963cb4126d6bd4bf7a585a1
kajal-thakur123 <kajal.kt383@gmail.com>
2024-03-19 17:22:36 +0530

    format ducomat page and  css file

--- src/screens/Calculator.tsx
+++ src/screens/Calculator.tsx
@@ -1,15 +1,17 @@
 import React, { useState } from "react";
 import "../styles/Calculator.scss";
-import userImage from '../assets/images/user.png';
-import dollarImage from '../assets/images/dollar.png';
+import userImage from "../assets/images/user.png";
+import dollarImage from "../assets/images/dollar.png";
+
 const Calculator = () => {
-  const defaultTipPercentage = "Custom"; 
-  const [billAmount, setBillAmount] = useState<any>('');
+
+  const [billAmount, setBillAmount] = useState<any>("");
   const [tipPercentage, setTipPercentage] = useState<any>("Custom");
   const [numberOfPeople, setNumberOfPeople] = useState<any>(null);
   const [customTipPercentage, setCustomTipPercentage] = useState<any>("");
   const [showCustomInput, setShowCustomInput] = useState(false);
-
+  const defaultTipPercentage = "Custom";
+  
   const handleBillAmountChange = (
     event: React.ChangeEvent<HTMLInputElement>
   ) => {
@@ -46,7 +48,6 @@ const Calculator = () => {
     setNumberOfPeople(parseInt(event.target.value));
   };
 
-
   const calculateTipAmount = () => {
     // Check if bill amount, number of people, and custom tip percentage are not 0 and not NaN
     if (
@@ -67,32 +68,31 @@ const Calculator = () => {
       if (!isNaN(tipAmount) && isFinite(tipAmount)) {
         return tipAmount.toFixed(2); // Fix to 2 decimal places
       } else {
-        return '0.00';
+        return "0.00";
       }
     }
     // If any of the values are 0 or NaN, return 0 tip amount
-    return '0.00';
+    return "0.00";
   };
-  
- 
+
   const handleReset = () => {
     // Reset all state values to their initial values
-    setBillAmount('');
+    setBillAmount("");
     setTipPercentage(defaultTipPercentage);
-    setNumberOfPeople('');
+    setNumberOfPeople("");
     setCustomTipPercentage("");
     setShowCustomInput(false);
-  
+
     // Remove active class from all tip percentage elements
     const tipPercentageElements = document.querySelectorAll(".tip-percentage");
-    tipPercentageElements.forEach(element => {
+    tipPercentageElements.forEach((element) => {
       element.classList.remove("active");
     });
   };
 
   const calculateTotalAmountPerPerson = () => {
     const tipAmount = parseFloat(calculateTipAmount());
-    
+
     // Check if bill amount, number of people, and custom tip percentage are not 0 and not NaN
     if (
       !isNaN(billAmount) &&
@@ -112,40 +112,42 @@ const Calculator = () => {
       }
     }
     // If any of the values are 0 or NaN, or the tip amount is not valid, return 0 total amount
-    return '0.00';
+    return "0.00";
   };
-  
 
   const tipPercentages = [5, 10, 15, 20, 25, "Custom"];
-  const resetButtonClassName =
-  parseFloat(calculateTipAmount()) === parseFloat('0') && parseFloat(calculateTotalAmountPerPerson()) === parseFloat('0')
-    ? "resetButton"
-    : "resetButton non-zero";
 
+  const resetButtonClassName =
+    parseFloat(calculateTipAmount()) === parseFloat("0") &&
+    parseFloat(calculateTotalAmountPerPerson()) === parseFloat("0")
+      ? "resetButton"
+      : "resetButton non-zero";
 
   return (
     <div className="calculator">
       <div className="main">
         <div className="calHeading">
-          <div className="calText">Splt</div> 
-          <div className="calText">tter</div> 
-          </div>
+          <div className="calText">Splt</div>
+          <div className="calText">tter</div>
+        </div>
         <div className="bottom">
           <div className="first">
             <div className="bill">
               <div className="heading">Bill</div>
-              
+
               <div className="billInput">
                 <input
                   type="text"
                   placeholder="0"
-                  className={`billAmountInput ${billAmount === 0 ? 'zero' : 'non-zero'}`}
+                  className={`billAmountInput ${
+                    billAmount === 0 ? "zero" : "non-zero"
+                  }`}
                   value={billAmount}
                   onChange={handleBillAmountChange}
                 />
-                         <div className="inputWithIcon">
-      <img src={dollarImage} alt="User Icon" className="userIcon" />
-      </div>
+                <div className="inputWithIcon">
+                  <img src={dollarImage} alt="User Icon" className="userIcon" />
+                </div>
               </div>
             </div>
             <div className="percentage">
@@ -154,7 +156,7 @@ const Calculator = () => {
                 className="tip-percentage-container"
                 style={{ textAlign: "center" }}
               >
-                {tipPercentages.map((percentage:any,index) =>
+                {tipPercentages.map((percentage: any, index) =>
                   percentage === "Custom" ? (
                     <div
                       key={index}
@@ -183,7 +185,9 @@ const Calculator = () => {
                     <input
                       type="text"
                       placeholder="0%"
-                      className={`billAmountInput ${customTipPercentage === 0 ? 'zero' : 'non-zero'}`}
+                      className={`billAmountInput ${
+                        customTipPercentage === 0 ? "zero" : "non-zero"
+                      }`}
                       value={customTipPercentage}
                       onChange={handleCustomTipChange}
                     />
@@ -199,47 +203,48 @@ const Calculator = () => {
                   <div className="error-message">Can't be zero</div>
                 )}
               </div>
-       
-      <div className="billInput">
-                
+
+              <div className="billInput">
                 <input
                   type="number"
                   placeholder="0"
-                  className={`billAmountInput ${numberOfPeople === 0 ? 'zero' : 'non-zero'}`}
+                  className={`billAmountInput ${
+                    numberOfPeople === 0 ? "zero" : "non-zero"
+                  }`}
                   value={numberOfPeople}
                   onChange={handleNumberOfPeopleChange}
                 />
-                       <div className="inputWithIcon">
-      <img src={userImage} alt="User Icon" className="userIcon" />
-      </div>
+                <div className="inputWithIcon">
+                  <img src={userImage} alt="User Icon" className="userIcon" />
+                </div>
               </div>
-
-              
             </div>
           </div>
-<div className="second">
-  <div className="total">
-    <div className="heading">
-      <div>
-        <div className="tip">Tip Amount</div>
-        <span>{numberOfPeople ? numberOfPeople : "0"}/person</span>
-      </div>
-      <div className="price">${parseFloat(calculateTipAmount()).toFixed(2)}</div>
-    </div>
-    <div className="heading">
-      <div>
-        <div className="total-text">Total</div>
-        <span>{numberOfPeople ? numberOfPeople : "0"}/person</span>
-      </div>
-      <div className="price">
-        ${parseFloat(calculateTotalAmountPerPerson()).toFixed(2)}
-      </div>
-    </div>
-  </div>
-  <div className="reset" onClick={handleReset}>
-    <button className={resetButtonClassName}>RESET</button>
-  </div>
-</div>
+          <div className="second">
+            <div className="total">
+              <div className="heading">
+                <div>
+                  <div className="tip">Tip Amount</div>
+                  <span>{numberOfPeople ? numberOfPeople : "0"}/person</span>
+                </div>
+                <div className="price">
+                  ${parseFloat(calculateTipAmount()).toFixed(2)}
+                </div>
+              </div>
+              <div className="heading">
+                <div>
+                  <div className="total-text">Total</div>
+                  <span>{numberOfPeople ? numberOfPeople : "0"}/person</span>
+                </div>
+                <div className="price">
+                  ${parseFloat(calculateTotalAmountPerPerson()).toFixed(2)}
+                </div>
+              </div>
+            </div>
+            <div className="reset" onClick={handleReset}>
+              <button className={resetButtonClassName}>RESET</button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
